c313ad0046ebf505bd0ec301b7a0894af7335c32
Enes Atik <padavatik@gmail.com>
2025-05-11 17:43:23 +0300

    use netlify

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const APP_URL = "tinyurl.com/story91";
-export const SERVER_URL = "https://pacific-server.onrender.com";
+export const SERVER_URL = "https://storyserver91.netlify.app/.netlify/functions/app";
 export const NUM_CHAPTERS = 5;
 export const REQUEST_NUM_TRIES = 5;
 export const REQUEST_WAIT_TIME = 300;
